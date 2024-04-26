4e16dd4f9ea4edaf52a28e303aaa06592fe18053
Casey Schneider-Mizell <caseysm@gmail.com>
2024-04-26 13:43:53 -0700

    use config file for example script

--- update_nvqueue.py
+++ update_nvqueue.py
@@ -11,19 +11,8 @@ from cavenvq import QueueReader
 @click.option("--config", "-c", help="Path to configuration file.")
 @click.option("--dry-run", "-d", default=False, is_flag=True, help="Run without actually updating.")
 def main(config, dry_run):
-    with open(config) as f:
-        settings = parse(f.read())
-    cv = CAVEclient(
-        datastack_name=settings["cave"]["datastack_name"],
-        server_address=settings["cave"]["server_address"],
-    )
-    nvq = NeuvueClient(
-        nuevue_url=settings["neuvue"]["url"],
-    )
-    qr = QueueReader(cv, nvq)
-    namespace = settings["neuvue"]["namespace"]
-    extra_sieve = settings["neuvue"].get("extra_sieve", {})
-    qr.run_update(nv_namespace=namespace, extra_sieve_filters=extra_sieve, dry_run=dry_run)
+    qr = QueueReader(config_file=config)
+    qr.run_update(dry_run=dry_run)
     if dry_run is False:
         logger.success(f"Checked nuevue queue with settings from {config}")
 
