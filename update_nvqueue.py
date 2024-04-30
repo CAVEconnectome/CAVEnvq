d8262d7d3c03d39732495fce82979b8e38de61b8
Casey Schneider-Mizell <caseysm@gmail.com>
2024-04-30 00:02:25 -0700

    use classfunction in script

--- update_nvqueue.py
+++ update_nvqueue.py
@@ -11,7 +11,7 @@ from cavenvq import QueueReader
 @click.option("--config", "-c", help="Path to configuration file.")
 @click.option("--dry-run", "-d", default=False, is_flag=True, help="Run without actually updating.")
 def main(config, dry_run):
-    qr = QueueReader(config_file=config)
+    qr = QueueReader.from_config(config)
     qr.run_update(dry_run=dry_run)
     if dry_run is False:
         logger.success(f"Checked nuevue queue with settings from {config}")
